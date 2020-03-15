269fec3ec27084d1040ef5c67933343755c04e3b
Michael Klishevich <busation@gmail.com>
2020-03-15 13:37:51 +0300

    class-transform

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,33 @@
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
+
 import { getName } from "./getName";
+import { Product } from "./product.model";
 
 const usetInput = "ddddd";
 const storedData = usetInput ?? "DEFAULT";
 
+declare var GLOBAL_MIKE: any;
+
 console.log(storedData);
 console.log(getName("Mike"));
+
+console.log(GLOBAL_MIKE);
+
+// const p1 = new Product("A Book", 12.99);
+// console.log(p1.getInformation());
+
+const products = [
+  { title: "A Carpet", price: 29.99 },
+  { title: "A Book", price: 12.99 }
+];
+
+// const loadedProducts = products.map(prod => {
+//   return new Product(prod.title, prod.price);
+// });
+
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
